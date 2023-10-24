93f7b4b4cf857a92334e9e24c4859a5d552351d8
Marvin Joseph <marvin.joseph.mj@gmail.com>
2023-10-24 13:29:47 +0700

    Change code server.js

--- server.js
+++ server.js
@@ -2,26 +2,16 @@ import express from "express";
 import cors from "cors";
 import "./loadEnv.js";
 import notes from "./routes/note.js";
-import { MongoClient } from "mongodb";
 
 const app = express();
-const PORT = process.env.PORT || 5050;
-
-const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("docs"));
 app.use("/note", notes);
 
-client.connect((err) => {
-  if (err) {
-    console.error(err);
-    return false;
-  }
-  // connection to mongo is successful, listen for requests
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
